d35035217a3abde5b44933e9ee8c1dd35ce27cf1
Harvie Torres <torresharviecaesar@gmail.com>
2026-03-16 13:05:57 +0800

    debug: catch startup error in wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,6 +1,16 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import traceback
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+try:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+except Exception as e:
+    traceback.print_exc()
+    def application(environ, start_response):
+        status = '500 Internal Server Error'
+        output = traceback.format_exc().encode('utf-8')
+        response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(output)))]
+        start_response(status, response_headers)
+        return [output]
