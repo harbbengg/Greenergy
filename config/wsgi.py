c792865c84a9ecdc3ab3c80c43abeac6501cd5e4
Harvie Torres <torresharviecaesar@gmail.com>
2026-03-16 11:37:16 +0800

    fix: force add staticfiles, wsgi auto-migrate, views syntax fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,20 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django.setup()
+
+# Run migrations automatically on startup
+from django.db import connection
+from django.db.migrations.executor import MigrationExecutor
+try:
+    executor = MigrationExecutor(connection)
+    if executor.migration_plan(executor.loader.graph.leaf_nodes()):
+        from django.core.management import call_command
+        call_command('migrate', '--noinput')
+except Exception as e:
+    print(f"Migration error: {e}")
 
 application = get_wsgi_application()
